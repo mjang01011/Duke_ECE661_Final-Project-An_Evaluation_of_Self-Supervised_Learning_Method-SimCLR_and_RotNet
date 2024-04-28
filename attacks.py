e4e2869eacef9230cb0b5c4329cbf966c3cfd745
Ethan H <32384354+EtHsu0@users.noreply.github.com>
2024-04-28 15:24:25 -0400

    [add] Upload result notebooks

--- attacks.py
+++ attacks.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 def random_noise_attack(model, device, dat, eps):
     x_adv = dat.clone().detach() + torch.FloatTensor(dat.shape).uniform_(-eps, eps).to(device)
-    x_adv = torch.clamp(x_adv.clone().detach(), 0., 1.)
+    x_adv = torch.clamp(x_adv.clone().detach(), -2.4291, 2.7537)
     return x_adv
 
 def gradient_wrt_data(model,device,data,lbl):
@@ -26,7 +26,7 @@ def PGD_attack(model, device, dat, lbl, eps, alpha, iters, rand_start):
         x_adv = random_noise_attack(model, device, x_nat, eps)
     else:
         x_adv = x_nat.clone()
-    x_adv = torch.clamp(x_adv, 0., 1.)
+    x_adv = torch.clamp(x_adv.clone().detach(), -2.4291, 2.7537)
     # print(x_nat)
     # print(torch.max(torch.abs(x_adv-x_nat)))
     for _ in range(iters):
@@ -34,29 +34,29 @@ def PGD_attack(model, device, dat, lbl, eps, alpha, iters, rand_start):
         x_adv = x_adv + alpha * torch.sign(data_grad)
         x_adv = torch.clamp(x_adv, -eps+x_nat, eps+x_nat)
         # print(torch.max(torch.abs(x_adv-x_nat)))
-        x_adv = torch.clamp(x_adv, 0., 1.)
+        x_adv = torch.clamp(x_adv.clone().detach(), -2.4291, 2.7537)
         # print("1clamp",torch.max(torch.abs(x_adv-x_nat)))
     return x_adv
 
 
 def FGSM_attack(model, device, dat, lbl, eps):
     x_nat = dat.clone().detach()
-    x_adv = torch.clamp(x_nat, 0., 1.)
+    x_adv = torch.clamp(x_nat.clone().detach(), -2.4291, 2.7537)
     data_grad = gradient_wrt_data(model, device, x_adv, lbl)
     x_adv = x_adv + eps * torch.sign(data_grad)
     x_adv = torch.clamp(x_adv, -eps+x_nat, eps+x_nat)
-    x_adv = torch.clamp(x_adv, 0., 1.)
+    x_adv = torch.clamp(x_adv.clone().detach(), -2.4291, 2.7537)
     return x_adv
 
 
 def rFGSM_attack(model, device, dat, lbl, eps):
     x_nat = dat.clone().detach()
     x_adv = random_noise_attack(model, device, x_nat, eps)
-    x_adv = torch.clamp(x_adv, 0., 1.)
+    x_adv = torch.clamp(x_nat.clone().detach(), -2.4291, 2.7537)
     data_grad = gradient_wrt_data(model, device, x_adv, lbl)
     x_adv = x_adv + eps * torch.sign(data_grad)
     x_adv = torch.clamp(x_adv, -eps+x_nat, eps+x_nat)
-    x_adv = torch.clamp(x_adv, 0., 1.)
+    x_adv = torch.clamp(x_adv.clone().detach(), -2.4291, 2.7537)
     return x_adv
 
 
